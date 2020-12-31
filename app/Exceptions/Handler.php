bd1972507f643c298d6c5bec0fe0fff87d274652
Vincent <vincent64e@gmail.com>
2020-12-31 14:49:26 +0800

    dd exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,8 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class Handler extends ExceptionHandler
 {
@@ -36,5 +38,11 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception){
+        dd($exception);
+
+        return parent::render($request, $exception);
     }
 }
