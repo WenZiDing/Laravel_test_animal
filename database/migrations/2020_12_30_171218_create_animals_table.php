2260dbf9b640ea86c103f95cfae59aedb7fee843
Vincent <vincent64e@gmail.com>
2021-01-02 23:13:19 +0800

    add table comment

--- database/migrations/2020_12_30_171218_create_animals_table.php
+++ database/migrations/2020_12_30_171218_create_animals_table.php
@@ -15,16 +15,16 @@ class CreateAnimalsTable extends Migration
     {
         Schema::create('animals', function (Blueprint $table) {
             $table->id(); // 此方法等於使用遞增整數設定一個ID欄位
-            $table->unsignedBigInteger('type_id')->nullable();
-            $table->string('name');
-            $table->date('birthday')->nullable();
-            $table->string('area')->nullable();
-            $table->boolean('fix')->default(false);
-            $table->text('description')->nullable();
-            $table->text('personality')->nullable();
-            $table->unsignedBigInteger('user_id');
-            $table->softDeletes();
-            $table->timestamps();
+            $table->unsignedBigInteger('type_id')->nullable()->comment('動物分類');
+            $table->string('name')->comment('動物暱稱');
+            $table->date('birthday')->nullable()->comment('生日');
+            $table->string('area')->nullable()->comment('所在地區');
+            $table->boolean('fix')->default(false)->comment('結紮情形');
+            $table->text('description')->nullable()->comment('簡單敘述');
+            $table->text('personality')->nullable()->comment('動物個性');
+            $table->unsignedBigInteger('user_id')->comment('所屬會員');
+            $table->softDeletes()->comment('刪除時間');
+            $table->timestamps()->comment('測試');
         });
     }
 
