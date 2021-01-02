e962f2bd2634c419e7eff941b75d90ea505c2806
Vincent <vincent64e@gmail.com>
2021-01-02 20:34:55 +0800

    add Request

--- app/Http/Controllers/AnimalController.php
+++ app/Http/Controllers/AnimalController.php
@@ -13,7 +13,7 @@ class AnimalController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
 				//
 			// $animal = Animal::get();
