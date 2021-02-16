827a7b54b885819d5b3d76d8c49bf7ff5f0bfe93
Vincent <vincent64e@gmail.com>
2021-02-16 08:23:34 +0800

    oauth

--- app/Http/Controllers/AnimalController.php
+++ app/Http/Controllers/AnimalController.php
@@ -14,6 +14,9 @@ class AnimalController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function __construct(){
+        $this->middleware('auth:api',['except'=>['index','show']]);
+    }
     public function index(Request $request)
     {
 				//
